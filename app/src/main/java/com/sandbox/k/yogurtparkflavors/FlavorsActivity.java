9c9da1e002e2e83e289be4d1102b838be97165be
Krystyn Neisess <krystynneisess@pioneers.berkeley.edu>
2016-12-30 00:24:08 -0800

    Fetch partial HTML page.
    Unfortunately, appears to require login of sorts for full page access (e.g. to posts).

--- app/src/main/java/com/sandbox/k/yogurtparkflavors/FlavorsActivity.java
+++ app/src/main/java/com/sandbox/k/yogurtparkflavors/FlavorsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,10 +16,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class FlavorsActivity extends AppCompatActivity {
     final String TAG_DEBUG = "TAG_DEBUG";
@@ -65,7 +71,9 @@ public class FlavorsActivity extends AppCompatActivity {
             try {
                 Document doc = getDocViaParse(urls[0]);
                 Element todaysPostElem = doc
-                        .select("div._5rgt._5nk5._5msi")
+//                        .select("div._5rgt._5nk5._5msi")
+//                        .select("div:contains(Flavors)")
+                        .select("div#viewport")
                         .first();
 
 //                Elements debugElems = doc.select("div._uag").first().children();
@@ -87,24 +95,154 @@ public class FlavorsActivity extends AppCompatActivity {
             return flavorData;
         }
 
-        private void updateHtmlAssetFile(String url) {
-            // TODO:  Fetch html page via http query.  Update local copy.
+        private InputStream getHtmlInputStream(String urlStr) throws IOException{
+            URL url = new URL(urlStr);
+            InputStream stream = null;
+            HttpsURLConnection connection = null;
+            try {
+                connection = (HttpsURLConnection) url.openConnection();
+                // Timeout for reading InputStream arbitrarily set to 3000ms.
+                connection.setReadTimeout(3000);
+                // Timeout for connection.connect() arbitrarily set to 3000ms.
+                connection.setConnectTimeout(3000);
+                // For this use case, set HTTP method to GET.
+                connection.setRequestMethod("GET");
+                // Already true by default but setting just in case; needs to be true since this request
+                // is carrying an input (response) body.
+                connection.setDoInput(true);
+                // Open communications link (network traffic occurs here).
+                connection.connect();
+                // Check response code for success.
+                int responseCode = connection.getResponseCode();
+                if (responseCode != HttpsURLConnection.HTTP_OK) {
+                    throw new IOException("HTTP error code: " + responseCode);
+                }
+                // Retrieve the response body as an InputStream.
+                stream = connection.getInputStream();
+
+            } finally {
+                // Disconnect HTTPS connection.  *Don't* close stream yet.
+                if (connection != null) {
+                    connection.disconnect();
+                }
+
+            }
+
+//            if (stream != null) {
+//                Log.d(TAG_DEBUG, "READING STREAM:  " + readStream(stream, 500));
+//            }
+
+            return stream;
         }
 
-        private File getHtmlAssetFile(String assetName, String url) throws IOException {
-            AssetManager assetManager = getAssets();
-            InputStream in = assetManager.open(assetName);
-            String outFilePath = context.getFilesDir().getPath() + "/html.tmp";
-            FileOutputStream out = new FileOutputStream(outFilePath);
 
-            int bytesRead = 0;
-            byte[] buff = new byte[1024];
-
-            while ((bytesRead = in.read(buff)) != -1) {
-                out.write(buff, 0, bytesRead);
+        private String readStream(InputStream stream, int maxLength) throws IOException {
+            String result = null;
+            // Read InputStream using the UTF-8 charset.
+            InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
+            // Create temporary buffer to hold Stream data with specified max length.
+            char[] buffer = new char[maxLength];
+            // Populate temporary buffer with Stream data.
+            int numChars = 0;
+            int readSize = 0;
+            while (numChars < maxLength && readSize != -1) {
+                numChars += readSize;
+                readSize = reader.read(buffer, numChars, buffer.length - numChars);
             }
-            in.close();
-            out.close();
+            if (numChars != -1) {
+                // The stream was not empty.
+                // Create String that is actual length of response body if actual length was less than
+                // max length.
+                numChars = Math.min(numChars, maxLength);
+                result = new String(buffer, 0, numChars);
+            }
+            return result;
+        }
+
+
+        private void updateHtmlFile(String filePath, String urlStr) {
+            // TODO:  Fetch html page via http query.  Update local copy.
+            InputStream in = null;
+            FileOutputStream out = null;
+            HttpsURLConnection connection = null;
+
+            try {
+                // get http request as an InputStream
+                URL url = new URL(urlStr);
+
+                connection = (HttpsURLConnection) url.openConnection();
+                // Timeout for reading InputStream arbitrarily set to 3000ms.
+                connection.setReadTimeout(3000);
+                // Timeout for connection.connect() arbitrarily set to 3000ms.
+                connection.setConnectTimeout(3000);
+                // For this use case, set HTTP method to GET.
+                connection.setRequestMethod("GET");
+                // Already true by default but setting just in case; needs to be true since this request
+                // is carrying an input (response) body.
+                connection.setDoInput(true);
+                // Open communications link (network traffic occurs here).
+                connection.connect();
+                // Check response code for success.
+                int responseCode = connection.getResponseCode();
+                if (responseCode != HttpsURLConnection.HTTP_OK) {
+                    throw new IOException("HTTP error code: " + responseCode);
+                }
+                // Retrieve the response body as an InputStream.
+                in = connection.getInputStream();
+
+                if (in != null) {
+                    Log.d(TAG_DEBUG, "READING STREAM:  " + readStream(in, 500));
+                }
+
+
+//                in = getHtmlInputStream(url);
+
+                // delete file if it already exists
+                File f = new File(filePath);
+                if (f.exists()) {
+                    f.delete();
+                }
+
+                // create new instance of file
+                f.createNewFile();
+
+                // write to new instance of file
+                out = new FileOutputStream(filePath);
+
+                int bytesRead = 0;
+                byte[] buff = new byte[1024];
+
+                while ((bytesRead = in.read(buff)) != -1) {
+                    out.write(buff, 0, bytesRead);
+                }
+            } catch(IOException e) {
+                Log.d(TAG_DEBUG, e.getMessage());
+            } finally {
+                // Disconnect HTTPS connection.  *Don't* close stream yet.
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+
+
+        }
+
+        private File getHtmlFile(String fileName, String url) throws IOException {
+            String outFilePath = "";
+
+            if (isExternalStorageReadable() && isExternalStorageWritable()) {
+                File externalFilesDir = context.getExternalFilesDir(null);
+                if (externalFilesDir != null) {
+                    outFilePath = externalFilesDir.getPath();
+                }
+            } else {
+                Log.d(TAG_DEBUG, "*** External Storage Unavailable!");
+                outFilePath = context.getFilesDir().getPath();
+            }
+            outFilePath +=  ("/" + fileName);
+
+            Log.d(TAG_DEBUG, "*** outFilePath:  " + outFilePath);
+            updateHtmlFile(outFilePath, url);
 
             return new File(outFilePath);
         }
@@ -119,10 +257,38 @@ public class FlavorsActivity extends AppCompatActivity {
         }
 
         private Document getDocViaParse(String url) throws IOException {
-            File inputFile = getHtmlAssetFile("m-yogurt-park.html", url);
+            File inputFile = getHtmlFile("m-yogurt-park.html", url);
 
             return Jsoup.parse(inputFile, "UTF-8", url);
         }
+
+
+
+
+        /*  Moooooaaaarr helpers. Courtesy of Android docu. */
+
+        /* Checks if external storage is available for read and write */
+        public boolean isExternalStorageWritable() {
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state)) {
+                return true;
+            }
+            return false;
+        }
+
+        /* Checks if external storage is available to at least read */
+        public boolean isExternalStorageReadable() {
+            String state = Environment.getExternalStorageState();
+            if (Environment.MEDIA_MOUNTED.equals(state) ||
+                    Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+                return true;
+            }
+            return false;
+        }
+
+
+
+
     }
 
 }
