158bbfa3ef7ea6a3a6f6c1a6356eb39a4ede6bae
Krystyn Neisess <krystynneisess@pioneers.berkeley.edu>
2017-01-01 21:30:59 -0800

    Remove all traces of Jsoup; begin switch to Facebook SDK.
    Specifically:
    - Remove all Jsoup code/dependencies.
    - Incorporate Facebook SDK build dependencies.

--- app/src/main/java/com/sandbox/k/yogurtparkflavors/FlavorsActivity.java
+++ app/src/main/java/com/sandbox/k/yogurtparkflavors/FlavorsActivity.java
@@ -2,7 +2,6 @@ package com.sandbox.k.yogurtparkflavors;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -10,18 +9,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import com.facebook.FacebookSdk;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -69,24 +63,8 @@ public class FlavorsActivity extends AppCompatActivity {
             String flavorData = "";
 
             try {
-                Document doc = getDocViaParse(urls[0]);
-                Element todaysPostElem = doc
-//                        .select("div._5rgt._5nk5._5msi")
-//                        .select("div:contains(Flavors)")
-                        .select("div#viewport")
-                        .first();
+                flavorData = getHttpResponse(urls[0]);
 
-//                Elements debugElems = doc.select("div._uag").first().children();
-
-//                flavorData = String.format("id:\n%s\nclass:\n%s",
-//                        todaysPostElem.id(),
-//                        todaysPostElem.className());
-                flavorData = todaysPostElem.text();
-//                for (Element e : debugElems) {
-//                    flavorData += e.className() + "*";
-//                }
-
-//                flavorData = todaysPostElem.id();
             } catch (IOException e) {
                 Log.d(TAG_DEBUG, e.getMessage());
                 flavorData = null;
@@ -95,10 +73,11 @@ public class FlavorsActivity extends AppCompatActivity {
             return flavorData;
         }
 
-        private InputStream getHtmlInputStream(String urlStr) throws IOException{
+        private String getHttpResponse(String urlStr) throws IOException{
             URL url = new URL(urlStr);
-            InputStream stream = null;
             HttpsURLConnection connection = null;
+            InputStream stream = null;
+            String streamAsString = null;
             try {
                 connection = (HttpsURLConnection) url.openConnection();
                 // Timeout for reading InputStream arbitrarily set to 3000ms.
@@ -119,11 +98,17 @@ public class FlavorsActivity extends AppCompatActivity {
                 }
                 // Retrieve the response body as an InputStream.
                 stream = connection.getInputStream();
+                // Convert to string
+                streamAsString = new Scanner(stream, "UTF-8").useDelimiter("\\A").next();
 
             } finally {
-                // Disconnect HTTPS connection.  *Don't* close stream yet.
+                // Disconnect HTTPS connection.
                 if (connection != null) {
                     connection.disconnect();
+                }
+
+                if (stream != null) {
+                    stream.close();
                 }
 
             }
@@ -132,10 +117,17 @@ public class FlavorsActivity extends AppCompatActivity {
 //                Log.d(TAG_DEBUG, "READING STREAM:  " + readStream(stream, 500));
 //            }
 
-            return stream;
+            return streamAsString;
         }
 
 
+
+
+
+
+        /*  Moooooaaaarr helpers. Courtesy of Android docu. */
+
+        /* Returns up to maxLength characters of stream as a String. */
         private String readStream(InputStream stream, int maxLength) throws IOException {
             String result = null;
             // Read InputStream using the UTF-8 charset.
@@ -159,114 +151,6 @@ public class FlavorsActivity extends AppCompatActivity {
             return result;
         }
 
-
-        private void updateHtmlFile(String filePath, String urlStr) {
-            // TODO:  Fetch html page via http query.  Update local copy.
-            InputStream in = null;
-            FileOutputStream out = null;
-            HttpsURLConnection connection = null;
-
-            try {
-                // get http request as an InputStream
-                URL url = new URL(urlStr);
-
-                connection = (HttpsURLConnection) url.openConnection();
-                // Timeout for reading InputStream arbitrarily set to 3000ms.
-                connection.setReadTimeout(3000);
-                // Timeout for connection.connect() arbitrarily set to 3000ms.
-                connection.setConnectTimeout(3000);
-                // For this use case, set HTTP method to GET.
-                connection.setRequestMethod("GET");
-                // Already true by default but setting just in case; needs to be true since this request
-                // is carrying an input (response) body.
-                connection.setDoInput(true);
-                // Open communications link (network traffic occurs here).
-                connection.connect();
-                // Check response code for success.
-                int responseCode = connection.getResponseCode();
-                if (responseCode != HttpsURLConnection.HTTP_OK) {
-                    throw new IOException("HTTP error code: " + responseCode);
-                }
-                // Retrieve the response body as an InputStream.
-                in = connection.getInputStream();
-
-                if (in != null) {
-                    Log.d(TAG_DEBUG, "READING STREAM:  " + readStream(in, 500));
-                }
-
-
-//                in = getHtmlInputStream(url);
-
-                // delete file if it already exists
-                File f = new File(filePath);
-                if (f.exists()) {
-                    f.delete();
-                }
-
-                // create new instance of file
-                f.createNewFile();
-
-                // write to new instance of file
-                out = new FileOutputStream(filePath);
-
-                int bytesRead = 0;
-                byte[] buff = new byte[1024];
-
-                while ((bytesRead = in.read(buff)) != -1) {
-                    out.write(buff, 0, bytesRead);
-                }
-            } catch(IOException e) {
-                Log.d(TAG_DEBUG, e.getMessage());
-            } finally {
-                // Disconnect HTTPS connection.  *Don't* close stream yet.
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-
-
-        }
-
-        private File getHtmlFile(String fileName, String url) throws IOException {
-            String outFilePath = "";
-
-            if (isExternalStorageReadable() && isExternalStorageWritable()) {
-                File externalFilesDir = context.getExternalFilesDir(null);
-                if (externalFilesDir != null) {
-                    outFilePath = externalFilesDir.getPath();
-                }
-            } else {
-                Log.d(TAG_DEBUG, "*** External Storage Unavailable!");
-                outFilePath = context.getFilesDir().getPath();
-            }
-            outFilePath +=  ("/" + fileName);
-
-            Log.d(TAG_DEBUG, "*** outFilePath:  " + outFilePath);
-            updateHtmlFile(outFilePath, url);
-
-            return new File(outFilePath);
-        }
-
-        private Document getDocViaConnect(String url) throws IOException {
-            return Jsoup.connect(url)
-                    .userAgent("Mozilla/5.0 (Linux; Android 6.0; LG-H810 Build/MRA58K) " +
-                            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.85 " +
-                            "Mobile Safari/537.36")
-                    .referrer("https://www.google.com")
-                    .get();
-        }
-
-        private Document getDocViaParse(String url) throws IOException {
-            File inputFile = getHtmlFile("m-yogurt-park.html", url);
-
-            return Jsoup.parse(inputFile, "UTF-8", url);
-        }
-
-
-
-
-        /*  Moooooaaaarr helpers. Courtesy of Android docu. */
-
         /* Checks if external storage is available for read and write */
         public boolean isExternalStorageWritable() {
             String state = Environment.getExternalStorageState();
@@ -285,10 +169,5 @@ public class FlavorsActivity extends AppCompatActivity {
             }
             return false;
         }
-
-
-
-
     }
-
 }
